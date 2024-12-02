f0ad08920266164f440d01f4a1995d6131df69ad
shubhParmar <100068784+shubhParmar@users.noreply.github.com>
2024-12-02 20:06:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-# POC 3
-# 1)	Create Modules for Azure Vnet, Azure Subnet, Resource Group and Static Web App.
-# 2)	Upload these modules to GitHub Repo.
-# 3)	Create calling code with remote Statefile as storage account container.
-# 4)	Upload calling code to GitHub.
-# 5)	Add these code to Github by Repo name terraform-azure-static-webapp
+# For POC details,see readme.md
 
 # creating storage account container for storing tfstate file
 terraform {
